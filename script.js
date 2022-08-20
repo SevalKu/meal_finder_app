494c40c1cc89c0ead5fabae8f0b9dceafa97f033
Seval Künler <knlr.swl@hotmail.com>
2022-08-21 00:37:00 +0300

    search button active

---
+++ script.js
@@ -0,0 +1,47 @@
+const search = document.getElementById("search");
+const submit = document.getElementById("submit");
+const random = document.getElementById("random");
+const mealsEl = document.getElementById("meals");
+const resultHeading = document.getElementById("result-heading");
+const single_mealEl = document.getElementById("single-meal");
+
+//search meal and fetch API
+function searchMeal(e){
+e.preventDefault();
+
+single_mealEl.innerHTML = ''
+
+const term = search.value;
+
+if (term.trim()){
+    fetch(`www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+    .then(res => res.json())
+    .then(data => {
+        resultHeading.innerHTML = `<h2>Search results for '${term}'</h2>`;
+
+
+        if(data.meals === null){
+            resultHeading.innerHTML = `<p>'There are no search results. Try again.</p>`
+        }else{
+            mealsEl.innerHTML = data.meals.map(meal=>`
+            <div class='meal'>
+            <img src='${meal.strMealThumb}' alt ='${meal.strMeal}'/>
+            <div class='meal-info' data-mealID='${meal.idMeal}'>
+            <h3>${meal.strMeal}</h3>
+            </div>
+            </div>`
+            )
+            .join('')
+        }
+    })
+    search.value=''
+
+}else {
+    alert('Please enter a search term')
+}
+}
+
+
+
+submit.addEventListener("submit", searchMeal);
+
